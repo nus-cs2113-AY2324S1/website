13de5d28c9fc892768001cf819b6cb43d4127e4b
Akshay Narayan <okkhoy@gmail.com>
2023-08-18 14:55:57 +0800

    Site Update. [skip ci]

--- schedule/week8/project.page-vue-render.js
+++ schedule/week8/project.page-vue-render.js
@@ -107,6 +107,6 @@ with(this){return _c('li',[_v("SUT: "),_c('a',{attrs:{"href":"https://github.com
 with(this){return _c('li',[_v("SUT: "),_c('a',{attrs:{"href":"https://github.com/se-edu/addressbook-level2/blob/master/src/seedu/addressbook/data/AddressBook.java"}},[_c('code',{pre:true,attrs:{"class":"line-numbers hljs inline no-lang"}},[_v("seedu.addressbook.data.AddressBook.java")])]),_c('br'),_v("\nTests: "),_c('a',{attrs:{"href":"https://github.com/se-edu/addressbook-level2/blob/master/test/java/seedu/addressbook/data/AddressBookTest.java"}},[_c('code',{pre:true,attrs:{"class":"line-numbers hljs inline no-lang"}},[_v("seedu.addressbook.data.AddressBookTest.java")])])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.0.1")]),_v(", generated on Fri, 18 Aug 2023, 14:44:06 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.0.1")]),_v(", generated on Fri, 18 Aug 2023, 14:53:54 GMT+8]")])])])}
 }];
   
